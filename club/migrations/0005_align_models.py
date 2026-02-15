570befe3ad283cce03254e55af455589582a3f19
KEIIZ-3 <iizukakenken@yahoo.co.jp>
2026-02-15 14:07:11 +0900

    Create 0005_align_models.py

---
+++ club/migrations/0005_align_models.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("club", "0004_add_coach_to_reservation"),
+    ]
+
+    operations = []
