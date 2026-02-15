1376a1494d3c37167587daeaa0537d3eb8ec7633
KEIIZ-3 <iizukakenken@yahoo.co.jp>
2026-02-15 17:30:30 +0900

    Create 0006_coachavailability_capacity.py

---
+++ club/migrations/0006_coachavailability_capacity.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("club", "0005_align_models"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="coachavailability",
+            name="capacity",
+            field=models.PositiveIntegerField(default=1),
+        ),
+    ]
