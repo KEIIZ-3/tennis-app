c95549e8d1ef7f06bd9299284064d9d741eda1bb
KEIIZ-3 <iizukakenken@yahoo.co.jp>
2026-02-16 13:50:41 +0900

    Update apps.py

--- club/apps.py
+++ club/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class ClubConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "club"
+
+    def ready(self):
+        from . import signals  # noqa
