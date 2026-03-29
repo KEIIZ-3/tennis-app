4f0ca461507c5af4b7748733037706d3b3ee4c0a
KEIIZ-3 <iizukakenken@yahoo.co.jp>
2026-03-30 07:50:04 +0900

    Add register path to urlpatterns

--- club/urls.py
+++ club/urls.py
@@ -6,6 +6,7 @@ app_name = "club"
 urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
     path("logout/", views.logout_view, name="logout"),
 
     # health check
