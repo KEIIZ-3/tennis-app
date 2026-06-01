d951f9dc7b0bf1f71ef76a1e9d92e6899316d377
KEIIZ-3 <iizukakenken@yahoo.co.jp>
2026-06-02 08:59:23 +0900

    Refactor settings.py to improve organization

--- tennisclub/settings.py
+++ tennisclub/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "club.preopen_level_policy.PreopenLevelFreeMiddleware",
     "club.middleware.AdminDashboardMenuMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
