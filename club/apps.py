d0c441e4f35c9013b85609c9aadce04449b0c301
KEIIZ-3 <iizukakenken@yahoo.co.jp>
2026-07-17 22:49:58 +0900

    Add settlement balance policy application

--- club/apps.py
+++ club/apps.py
@@ -11,7 +11,11 @@ class ClubConfig(AppConfig):
         from .capacity_guard import apply_reservation_capacity_guard
         from .court_type_policy import apply_court_type_policy
         from .preopen_level_policy import apply_preopen_level_policy
+        from .settlement_balance_policy import (
+            apply_settlement_balance_policy,
+        )
 
         apply_court_type_policy()
         apply_preopen_level_policy()
         apply_reservation_capacity_guard()
+        apply_settlement_balance_policy()
